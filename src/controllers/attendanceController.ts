59ac533d56bd083a6e33c691911130272a36a359
karim Mohamed <karimdesouki147@gmail.com>
2024-06-30 14:20:09 +0300

    refactor: Removed all the comments and console logs

--- src/controllers/attendanceController.ts
+++ src/controllers/attendanceController.ts
@@ -8,29 +8,6 @@ import EmployeeService from '../services/employeeService';
 const employeeService = new EmployeeService(new EmployeeRepository());
 
 /*----------------------------Checkin-------------------------*/
-
-// export const checkIn = async (req: Request, res: Response) => {
-//   const { employeeId } = req.body;
-//   try {
-//     if (!employeeId) {
-//       return res.status(400).json({ error: 'Employee ID is required' });
-//     }
-//     const employee = await EmployeeRepository.getById(employeeId);
-//     if (!employee) {
-//       return res
-//         .status(404)
-//         .json({ error: `Employee ID ${employeeId} Not found` });
-//     }
-//     await AttendanceRepository.checkIn(employeeId);
-//     return res
-//       .status(200)
-//       .json({ message: `Checked In successfully for ID: ${employeeId}` });
-//   } catch (error) {
-//     console.error(`Error while Checking in for ID ${employeeId}`, error);
-//     return res.status(500).json({ error: 'Internal Server Error!!' });
-//   }
-// };
-
 export const checkIn = async (req: Request, res: Response) => {
   const { employeeId } = req.body;
   try {
@@ -81,30 +58,6 @@ export const checkOut = async (req: Request, res: Response) => {
   }
 };
 /*----------------------------getDailyHours-------------------------*/
-// export const getDailyHours = async (req: Request, res: Response) => {
-//   const { employeeId } = req.query;
-//   const date = new Date(req.query.date as string);
-//   try {
-//     if (!employeeId) {
-//       return res.status(400).json({ error: 'Employee ID is required' });
-//     }
-//     const totalHours = await AttendanceRepository.getDailyHours(
-//       parseInt(employeeId as string),
-//       date,
-//     );
-//     return res.status(200).json({
-//       employeeId,
-//       date: date.toISOString().split('T')[0],
-//       totalHours,
-//     });
-//   } catch (error) {
-//     console.error(
-//       `Error while fetching daily hours for ID: ${employeeId}`,
-//       error,
-//     );
-//     return res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
 export const getDailyHours = async (req: Request, res: Response) => {
   const { employeeId } = req.query;
   const date = req.query.date ? new Date(req.query.date as string) : new Date();
@@ -133,33 +86,6 @@ export const getDailyHours = async (req: Request, res: Response) => {
   }
 };
 /*----------------------------getMonthlyHours-------------------------*/
-// export const getMonthlyHours = async (req: Request, res: Response) => {
-//   const { employeeId, year, month } = req.query;
-//   try {
-//     if (!employeeId || !year || !month) {
-//       return res
-//         .status(400)
-//         .json({ error: 'Employee ID, year, and month are required' });
-//     }
-//     const employeeExists = await employeeService.getById(Number(employeeId));
-//     if (!employeeExists) {
-//       return res.status(404).json({ error: 'Employee not found' });
-//     }
-//     const { hours, salary } = await AttendanceRepository.getMonthlyHours(
-//       Number(employeeId),
-//       Number(year),
-//       Number(month),
-//     );
-//     return res.status(200).json({ hours, salary });
-//   } catch (error) {
-//     console.error(
-//       `Error fetching monthly hours for employee ID: ${employeeId}`,
-//       error,
-//     );
-//     return res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
 export const getMonthlyHours = async (req: Request, res: Response) => {
   const { employeeId, year, month, current } = req.query;
   try {

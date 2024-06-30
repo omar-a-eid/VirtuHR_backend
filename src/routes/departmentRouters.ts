624bc5f539b66f3bf7ae6f8b80bfb748dcfec321
karim Mohamed <karimdesouki147@gmail.com>
2024-06-30 04:56:19 +0300

    refactor: Updated changes in Add Employee Form

--- src/routes/departmentRouters.ts
+++ src/routes/departmentRouters.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 // router.get('/departments/:id', DepartmentController.getById);
 router.get('/departments/employees/:id', DepartmentController.getEmployees);
+router.get('/departments/employees', DepartmentController.getAll);
 
 export default router;
